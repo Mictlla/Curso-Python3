554a03bb33e835bbb6ed996d97e17d1b3bbf815d
Mictlla <ashaskty7@gmail.com>
2020-10-14 17:36:40 -0500

    Ejercicios e python3 hasta antes del proyecto palindromoZ

--- conversor-moneda.py
+++ conversor-moneda.py
@@ -1,7 +1,31 @@
-pesos = input("¿Cuantos pesos mexicanos tienes: ")
-pesos = float(pesos)
-valor_dolar = 21
-dolares = pesos / valor_dolar
-dolares = round(dolares, 2)
-dolares = str(dolares)
-print("Tienes $" + dolares + " dolares")
+def conversor(tipo_pesos, valor_dolar):
+    pesos = input("¿Cuantos pesos " + tipo_pesos + " tienes?: ")
+    pesos = float(pesos)
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dolares")
+
+
+
+menu = """ 
+Bienvenido al conversor de monedas 💰
+1- Pesos mexicanos
+2- Pesos colombianos
+3- Pesos chilenos
+
+Elige una opcion: 
+"""
+
+opcion = input(menu)
+
+if opcion == "1":
+    conversor("mexicanos", 21)
+elif opcion == "2":
+    conversor("colombianos", 3836)    
+elif opcion == "3":
+    conversor("chilemos", 798.80)
+else:
+    print("Ingresa una opcion correcta por favor")
+
+
